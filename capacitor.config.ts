85c6c562660d63fca04311658b0affeb690917c1
HakeemAzeez <22ftt1511@student.pb.edu.bn>
2024-05-18 12:17:33 +0800

    bla bla

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,24 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'splash screen 3',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      // launchAutoHide: true,
+      backgroundColor: "#de0f17",
+      // androidSplashResiurceName: "splash",
+      // androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      androidSpinnerStyle: "small",
+      iosSpinnerStyle: "small",
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+  },
 };
 
 export default config;
